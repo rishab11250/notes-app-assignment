b9008eafdcd98a497f09f77a6e1d8531b4d05d12
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-04-16 17:07:42 +0530

    feat: get all note endpoint(GET /api/notes)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -23,4 +23,14 @@ const createBulkNotes = async(req,res) =>{
     }
 }
 
-module.exports = { createNote, createBulkNotes };
+const getAllNotes = async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.status(200).json({success: true, message: 'Notes fetched successfully', data: notes });
+    } 
+    catch (err) {
+        res.status(500).json({success: false, message: 'Error retrieving notes', data: err });
+    }
+};
+
+module.exports = { createNote, createBulkNotes, getAllNotes };
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const {
     createNote,
-    createBulkNotes
+    createBulkNotes,
+    getAllNotes
 } = require('../controllers/note.controller');
 
 // CRUD routes
 router.post('/', createNote);
 router.post('/bulk', createBulkNotes);
+router.get('/', getAllNotes);
 
 module.exports = router;
